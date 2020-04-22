887df461283be50f2685963120a9c5795dbe00a3
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-22 00:00:29 -0700

    Update.001

---
+++ css/js/index_scripts.js
@@ -0,0 +1,35 @@
+// var emailIcon = document.getElementById("email");
+// var igIcon = document.getElementById("ig");
+// var facebookIcon = document.getElementById("facebook");
+// var navBtn = document.getElementById("nav-btn");
+
+
+document.getElementById("nav-btn").onclick = function() {navOpen()};
+
+function navOpen() {
+  document.getElementById("email").className = "email-on";
+  document.getElementById("ig").className = "ig-on";
+  document.getElementById("facebook").className = "facebook-on";
+}
+
+document.getElementById("left-arrow").onclick = function() {infoOpenClose()};
+
+function infoOpenClose() {
+  if (document.getElementById("left-arrow").className == "left-arrow"){
+    document.getElementById("info").className = "info-on";
+    document.getElementById("left-arrow").className = "right-arrow";
+  }
+  else {
+    document.getElementById("info").className = "info-off";
+    document.getElementById("left-arrow").className = "left-arrow";
+  }
+}
+
+
+
+// navBtn.onclick = function() {toggleOff()};
+//   function toggleOff(){
+//     emailIcon.classList.toggle("hidden");
+//     igIcon.classList.toggle("hidden");
+//     facebookIcon.classList.toggle("hidden");
+//   }
