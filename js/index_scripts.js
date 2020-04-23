beeaa1bc66a6ff30c592d70793ab5d3eb3146798
Cheyne Forgerson <forgerson.dev@gmail.com>
2020-04-22 23:44:02 -0700

    Update.006
    Added about /
    Viewport height test w/JS /

--- js/index_scripts.js
+++ js/index_scripts.js
@@ -3,6 +3,15 @@
 // var facebookIcon = document.getElementById("facebook");
 // var navBtn = document.getElementById("nav-btn");
 
+// -------VIEWPORT HEIGHT-------
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 document.getElementById("nav-btn").onclick = function() {navOpen()};
 
@@ -18,13 +27,13 @@ function infoOpenClose() {
   if (document.getElementById("left-arrow").className == "left-arrow"){
     document.getElementById("info").className = "info-on";
     document.getElementById("left-arrow").className = "left-arrow-fade";
-    setTimeout(rightArrowClass, 750);
+    setTimeout(rightArrowClass, 500);
 
   }
   else {
     document.getElementById("info").className = "info-off";
     document.getElementById("left-arrow").className = "right-arrow-fade";
-    setTimeout(leftArrowClass, 750);
+    setTimeout(leftArrowClass, 500);
   }
 }
 
